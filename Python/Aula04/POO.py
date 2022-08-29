a95b32b7664a283f94366151c97370a4bb382977
Luan Lara de Moraes <luanlaramoraes@gmail.com>
2022-08-29 14:21:42 -0300

    Aula 04 - POO

---
+++ Python/Aula04/POO.py
@@ -0,0 +1,36 @@
+class Pessoa:
+    #"def" é usado para definir função em Python, é o início de um método
+    # O "self" é usado como o "this" em java
+    #"init" é o construtor
+    def __init__(self, altura, cor_cabelo, peso):
+        self.altura = altura
+        self.cor_cabelo = cor_cabelo
+        self.peso = peso 
+
+    def falar(self, frase):
+        return frase
+
+    def andar(self, velocidade = 5):
+        velocidade_real = velocidade - 0.01 * self.peso
+        return velocidade_real
+    
+# Herança
+class Professor(Pessoa):
+    def __init__(self, altura, cor_cabelo, peso, salario):
+        super().__init__(altura, cor_cabelo, peso)
+        self.salario = salario
+
+    def falar(self, frase):
+        return frase + ' Vai cair na prova!'
+
+def main():
+    pessoaObj = Pessoa(1.8, 'Castanho', 95.0)
+    print(pessoaObj.falar("Python é legal."))
+    print(pessoaObj.andar(7))
+
+    professorObj = Professor(1.9, 'Loiro', 89.0, 2000.0)
+    print(professorObj.falar('Python é top.'))
+    print(professorObj.andar(9))
+
+if __name__ == '__main__':
+    main()
